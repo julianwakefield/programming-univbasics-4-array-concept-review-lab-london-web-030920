2106fee3866d61f1c2cd3c9662edc26697c0e48b
Julian Wakefield <julianwakefield@outlook.com>
2020-02-28 16:58:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ array.index(value_to_find)
 end
 
 def find_max_value(array)
-max_value = array[0]
+max_value = 0
 array.length.times { |index|
 if array[index] > max_value
 max_value = array[index]
@@ -12,5 +12,10 @@ max_value
 end
 
 def find_min_value(array)
-
+min_value = 1
+array.length.times { |index|
+if array[index] < min_value
+  min_value += array[index]
+end}
+min_value
 end

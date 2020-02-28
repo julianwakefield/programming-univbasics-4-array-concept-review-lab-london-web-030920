50856e227416907f10cdd3bc5728f2298f5c01d4
Julian Wakefield <julianwakefield@outlook.com>
2020-02-28 19:20:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,9 +13,10 @@ end
 
 def find_min_value(array)
 min_value = 0
-array.length.times { |index|
+array.length.times do |index|
 if array[index] < min_value
   min_value = array[index]
-end}
+end
+end
 min_value
 end
